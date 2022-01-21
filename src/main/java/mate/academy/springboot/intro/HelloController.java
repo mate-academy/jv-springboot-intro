9a32b5f7d6fd79e599fc1976f23c7183cde17d1b
nishchenkokateryna <kateryna.nishchenko@gmail.com>
2022-01-21 13:32:42 +0200

    Method name changedэ

--- src/main/java/mate/academy/springboot/intro/HelloController.java
+++ src/main/java/mate/academy/springboot/intro/HelloController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/hello")
 public class HelloController {
     @GetMapping
-    public String hello() {
+    public String sayHello() {
         return "Hello, mates!";
     }
 }
