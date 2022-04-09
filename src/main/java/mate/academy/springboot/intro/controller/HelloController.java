86d90abf2152eb6e4bd37661f89e1c0f37197161
Liliia Ponomarenko <liliya97ua@gmail.com>
2022-04-09 17:15:13 +0300

    added HelloController

--- src/main/java/mate/academy/springboot/intro/controller/HelloController.java
+++ src/main/java/mate/academy/springboot/intro/controller/HelloController.java
@@ -5,10 +5,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
-
     @GetMapping("/hello")
     public String sayHello() {
         return "Hello, mates!";
     }
-
 }
