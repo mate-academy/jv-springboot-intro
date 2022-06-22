b52460549e07cc11de480db83b4c982f6e059e4b
dombrovskiy2850@gmail.com <dombrovskiy2850@gmail.com>
2022-06-22 23:28:18 +0300

    second commit

--- src/main/java/mate/academy/springboot/intro/controller/HelloController.java
+++ src/main/java/mate/academy/springboot/intro/controller/HelloController.java
@@ -1,12 +1,15 @@
-package mate.academy.springboot.intro;
+package mate.academy.springboot.intro.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/hello")
 public class HelloController {
-    @GetMapping("hello")
-    public String sayHello() {
+
+    @GetMapping
+    public String returnHello() {
         return "Hello, mates!";
     }
 }
