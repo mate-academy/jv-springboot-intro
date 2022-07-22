06a33432c76be249a97e46f87d817d151725bc82
Vitalii Bozadzhy <VitaliiBozadzhy@icloud.com>
2022-07-22 11:34:34 +0200

    -added solution

---
+++ src/main/java/mate/academy/springboot/intro/HelloController.java
@@ -0,0 +1,19 @@
+package mate.academy.springboot.intro;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    private final MateService service;
+
+    public HelloController(MateService service) {
+        this.service = service;
+    }
+
+    @GetMapping("/hello")
+    public String sayHi() {
+        return service.getKey();
+    }
+
+}
