313450d5048ce0c966000dda5e2b4c1947a17acb
Dasha Horun <dhorunka@gmail.com>
2022-11-02 19:36:27 +0200

    checkstyle is done

--- src/main/java/mate/academy/springboot/intro/controller/HelloController.java
+++ src/main/java/mate/academy/springboot/intro/controller/HelloController.java
@@ -6,8 +6,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
 
-     @GetMapping("/hello")
-    public String getHello(){
-         return "Hello, mates!";
-     }
+    @GetMapping("/hello")
+    public String getHello() {
+        return "Hello, mates!";
+    }
 }
