1b99e8ed697227f1366d9234f4eadc2302b982e4
Aptypio13 <aptypio668@gmail.com>
2022-11-04 17:43:11 +0200

    fix checkstyle

--- src/main/java/mate/academy/springboot/intro/controller/HelloController.java
+++ src/main/java/mate/academy/springboot/intro/controller/HelloController.java
@@ -1,7 +1,6 @@
 package mate.academy.springboot.intro.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
