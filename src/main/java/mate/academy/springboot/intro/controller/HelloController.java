9a5ca7d24dda5482d495f9559194872a57543b1a
Danylo Borysenko <borysenkodanyl@gmail.com>
2023-04-08 21:06:49 +0300

    HelloController was created

---
+++ src/main/java/mate/academy/springboot/intro/controller/HelloController.java
@@ -0,0 +1,14 @@
+package mate.academy.springboot.intro.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloController {
+    @GetMapping
+    public String sayHello() {
+        return "Hello, mates!";
+    }
+}
